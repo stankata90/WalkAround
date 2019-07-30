6021bfdf675a0658672047ba40edf9f40151f273
stankata90 <stanislav.vasilev.90@gmail.com>
2019-07-30 11:48:22 +0300

    Update Bundle Name

--- src/WalkAroundBundle/Controller/UserController.php
+++ src/WalkAroundBundle/Controller/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace WalkAroundBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
---
+++ src/WalkAroundBundle/WalkAroundBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WalkAroundBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WalkAroundBundle extends Bundle
+{
+}
